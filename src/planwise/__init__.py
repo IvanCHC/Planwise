9afdd8d97eeda1666fbeb418ec86ca3330b8f173
ivanchc <ivanchanny1997@gmail.com>
2025-08-02 15:11:50 +0100

    Add a blank line for improved readability in __init__.py

--- src/planwise/__init__.py
+++ src/planwise/__init__.py
@@ -5,6 +5,7 @@ A Python library for modeling retirement savings across various UK tax wrappers
 including Lifetime ISA (LISA), Stocks & Shares ISA, Self-Invested Personal
 Pension (SIPP) and workplace pensions.
 """
+
 from .core import project_retirement
 from .plotting import make_contribution_plot, make_growth_plot
 from .tax import TaxBand, calculate_income_tax, get_tax_bands
