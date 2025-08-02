7447cdba891ee4962d7001abb8d101323e14e13c
ivanchc <ivanchanny1997@gmail.com>
2025-08-02 05:25:57 +0100

    Add initial implementation of Planwise library and test directory

---
+++ src/planwise/__init__.py
@@ -0,0 +1,10 @@
+"""
+Planwise - UK Investment & Retirement Planning Library.
+
+A Python library for modeling retirement savings across various UK tax wrappers
+including Lifetime ISA (LISA), Stocks & Shares ISA, Self-Invested Personal
+Pension (SIPP) and workplace pensions.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Planwise Team"
