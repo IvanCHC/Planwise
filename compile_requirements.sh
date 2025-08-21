d8c066f0277d8b4bf5b08e56cd3aaa52dad96964
ivanchc <ivanchanny1997@gmail.com>
2025-08-21 17:38:36 +0100

    Update requirements generation to include all extras

--- compile_requirements.sh
+++ compile_requirements.sh
@@ -2,6 +2,6 @@
 
 echo "Updating requirements.txt"
 uv sync
-uv pip compile pyproject.toml -o requirements.txt >/dev/null
+uv pip compile pyproject.toml --all-extras -o requirements.txt >/dev/null
 echo "-e ." >>requirements.txt
 echo "Successfully updated requirements.txt"
